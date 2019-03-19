980082ef544c1396eadfe6bf33147f7ead4c55e9
Duncan Hall <duncandhall@gmail.com>
2019-03-18 22:17:27 -0400

    simplesampler is broken

---
+++ simplesampler/Source/PluginProcessor.cpp
@@ -0,0 +1,171 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+SimplesamplerAudioProcessor::SimplesamplerAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+}
+
+SimplesamplerAudioProcessor::~SimplesamplerAudioProcessor()
+{
+}
+
+//==============================================================================
+const String SimplesamplerAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool SimplesamplerAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool SimplesamplerAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool SimplesamplerAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double SimplesamplerAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int SimplesamplerAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int SimplesamplerAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void SimplesamplerAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const String SimplesamplerAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void SimplesamplerAudioProcessor::changeProgramName (int index, const String& newName)
+{
+}
+
+//==============================================================================
+void SimplesamplerAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+    synth.setCurrentPlaybackSampleRate(sampleRate);
+}
+
+void SimplesamplerAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool SimplesamplerAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    if (layouts.getMainOutputChannelSet() != AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void SimplesamplerAudioProcessor::processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiMessages)
+{
+    std::cout << midiMessages.getFirstEventTime();  // doesn't work
+    std::cout << "\n";
+    synth.renderNextBlock(buffer, midiMessages, 0, buffer.getNumSamples());
+}
+
+//==============================================================================
+bool SimplesamplerAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+AudioProcessorEditor* SimplesamplerAudioProcessor::createEditor()
+{
+    return new SimplesamplerAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void SimplesamplerAudioProcessor::getStateInformation (MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void SimplesamplerAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new SimplesamplerAudioProcessor();
+}
+
